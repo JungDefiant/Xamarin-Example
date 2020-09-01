a4595f93afced151560162bc168e93edfe0cce91
JungDefiant <jungdefiant@gmail.com>
2020-08-31 17:34:41 -0700

    Initial commit

---
+++ Example-Xamarin-App/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Example_Xamarin_App
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+
+            await Navigation.PushAsync(new NameEntryPage());
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
---
+++ Example-Xamarin-App/NameEntryPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Example_Xamarin_App
+{ 
+    public partial class NameEntryPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        ObservableCollection<TextString> text = new ObservableCollection<TextString>();
+        public ObservableCollection<TextString> Text { get { return text; } }
+
+        public NameEntryPage()
+        {
+            InitializeComponent();
+            if (File.Exists(_fileName))
+            {
+                var arr = File.ReadAllLines(_fileName);
+
+                foreach (var str in arr)
+                {
+                    //text.Add(new TextString { Line = str } );
+                    text.Add(new TextString { Line = str });
+                }
+
+                //ListView List = new ListView();
+                List.ItemsSource = text;
+            }
+        }
+
+        public class TextString
+        {
+            public string Line { get; set; }
+        }
+    }
+}
